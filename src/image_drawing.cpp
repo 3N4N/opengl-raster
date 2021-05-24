fa88f72427969f231cde3f8783540b7025341700
Enan Ajmain <3nan.ajmain@gmail.com>
2021-05-24 21:46:34 +0600

    Draw triangles with z-buffer algorithm

--- src/image_drawing.cpp
+++ src/image_drawing.cpp
@@ -4,7 +4,7 @@
 *****************************************
 */
 
-#include "bitmap_image.hpp"
+#include "bitmap_image.h"
 
 using namespace std;
 
@@ -12,24 +12,25 @@ using namespace std;
 int main(){
 
     bitmap_image image(500,300);
+    image.set_all_channels(0, 0, 255);
 
-    for(int i=0;i<200;i++){
-        for(int j=0;j<100;j++){
+    for(int i=0;i<20;i++){
+        for(int j=0;j<10;j++){
             image.set_pixel(i,j,255,255,0);
         }
-        for(int j=150;j<200;j++){
-            image.set_pixel(i,j,0,0,255);
-        }
+        // for(int j=150;j<200;j++){
+        //     image.set_pixel(i,j,0,0,255);
+        // }
     }
 
-    for(int i=400;i<450;i++){
-        for(int j=50;j<150;j++){
-            image.set_pixel(i,j,255,0,0);
-        }
-        for(int j=200;j<300;j++){
-            image.set_pixel(i,j,0,255,255);
-        }
-    }
+    // for(int i=400;i<450;i++){
+    //     for(int j=50;j<150;j++){
+    //         image.set_pixel(i,j,255,0,0);
+    //     }
+    //     for(int j=200;j<300;j++){
+    //         image.set_pixel(i,j,0,255,255);
+    //     }
+    // }
 
     image.save_image("test.bmp");;
 
