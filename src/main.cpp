07fbbeab21e0e918b9bf76a94e7164c1296956eb
Enan Ajmain <3nan.ajmain@gmail.com>
2021-05-26 09:25:52 +0600

    Use row and height instead of y and x coordinates

--- src/main.cpp
+++ src/main.cpp
@@ -17,20 +17,21 @@ using namespace std;
 // #define PI (2 * acos(0.0))
 #define RAD(t) (t * (2 * acos(0.0)) / 180.0)
 
-string nm_scenefile = "test_cases/1/scene.txt";
-string nm_conffile  = "test_cases/1/config.txt";
+string nm_scenefile = "test_cases/3/scene.txt";
+string nm_conffile  = "test_cases/3/config.txt";
 string nm_stage1file = "bin/stage1.txt";
 string nm_stage2file = "bin/stage2.txt";
 string nm_stage3file = "bin/stage3.txt";
+string nm_zbuffile = "bin/z_buffer.txt";
 
 int screen_width;
 int screen_height;
-double lim_xl;
-double lim_xr;
-double lim_yb;
-double lim_yt;
-double lim_zf;
-double lim_zr;
+float lim_xl;
+float lim_xr;
+float lim_yb;
+float lim_yt;
+float lim_zf;
+float lim_zr;
 
 rgb_store rainbow[6] = {
     {255,0,0},      // red
@@ -41,7 +42,7 @@ rgb_store rainbow[6] = {
     {139,0,255}     // violet
 };
 
-typedef std::vector<std::vector<double> > vec2d;
+typedef std::vector<std::vector<float> > vec2d;
 
 void print_matrix(const vec2d &mat)
 {
@@ -65,7 +66,7 @@ void identity_matrix(vec2d &mat)
 
 vec2d multiply(const vec2d &matA, const vec2d &matB)
 {
-    vec2d res(matA.size(), vector<double>(matB[0].size(), 0));
+    vec2d res(matA.size(), vector<float>(matB[0].size(), 0));
     for (int i = 0; i < res.size(); i++) {
         for (int j = 0; j < res[0].size(); j++) {
             for (int k = 0; k < 4; k++)
@@ -78,9 +79,9 @@ vec2d multiply(const vec2d &matA, const vec2d &matB)
 Point transformPoint(const vec2d &matrix, const Point &point)
 {
     Point p;
-    double w;
+    float w;
 
-    vec2d p_mat(4, vector<double>(1,0));
+    vec2d p_mat(4, vector<float>(1,0));
     p_mat[0][0] = point.x;
     p_mat[1][0] = point.y;
     p_mat[2][0] = point.z;
@@ -94,10 +95,10 @@ Point transformPoint(const vec2d &matrix, const Point &point)
     return p;
 }
 
-Point rodrigues(Point x, Point a, double angle) {
+Point rodrigues(Point x, Point a, float angle) {
 
-    double cost = cos(RAD(angle));
-    double sint = sin(RAD(angle));
+    float cost = cos(RAD(angle));
+    float sint = sin(RAD(angle));
 
 
     return multiply(x, cost)
@@ -140,7 +141,7 @@ int main()
     stage3file << setprecision(7) << fixed;
 
     Point eye, look, up;
-    double fovY, aspect_ratio, near, far;
+    float fovY, aspect_ratio, near, far;
 
     scenefile >> eye;
     scenefile >> look;
@@ -153,13 +154,13 @@ int main()
     normalize(r);
     Point u = cross_product(r, l);
 
-    vec2d T(4, vector<double>(4,0));
+    vec2d T(4, vector<float>(4,0));
     identity_matrix(T);
     T[0][3] = -eye.x;
     T[1][3] = -eye.y;
     T[2][3] = -eye.z;
 
-    vec2d R(4, vector<double>(4,0));
+    vec2d R(4, vector<float>(4,0));
     R[0][0] =  r.x; R[0][1] =  r.y; R[0][2] =  r.z;
     R[1][0] =  u.x; R[1][1] =  u.y; R[1][2] =  u.z;
     R[2][0] = -l.x; R[2][1] = -l.y; R[2][2] = -l.z;
@@ -167,11 +168,11 @@ int main()
 
     vec2d V = multiply(R, T);
 
-    double fovX = fovY * aspect_ratio;
-    double _t = near * tan(RAD(fovY / 2));
-    double _r = near * tan(RAD(fovX / 2));
+    float fovX = fovY * aspect_ratio;
+    float _t = near * tan(RAD(fovY / 2));
+    float _r = near * tan(RAD(fovX / 2));
 
-    vec2d P(4, vector<double>(4,0));
+    vec2d P(4, vector<float>(4,0));
     P[0][0] = near/_r;
     P[1][1] = near/_t;
     P[2][2] = -(far+near)/(far-near);
@@ -179,7 +180,7 @@ int main()
     P[3][2] = -1;
     // print_matrix(P);
 
-    vec2d mat_identity(4, vector<double>(4,0));
+    vec2d mat_identity(4, vector<float>(4,0));
     identity_matrix(mat_identity);
 
     string command;
@@ -205,21 +206,21 @@ int main()
             stage2file << "\n";
             stage3file << "\n";
         } else if (command == "translate") {
-            vec2d tran_mat(4, vector<double>(4,0));
+            vec2d tran_mat(4, vector<float>(4,0));
             identity_matrix(tran_mat);
             scenefile >> tran_mat[0][3] >> tran_mat[1][3] >> tran_mat[2][3];
             vec2d mat = multiply(S.top(), tran_mat);
             S.pop();
             S.push(mat);
         } else if (command == "scale") {
-            vec2d scale_mat(4, vector<double>(4,0));
+            vec2d scale_mat(4, vector<float>(4,0));
             scenefile >> scale_mat[0][0] >> scale_mat[1][1] >> scale_mat[2][2];
             scale_mat[3][3] = 1;
             vec2d mat = multiply(S.top(), scale_mat);
             S.pop();
             S.push(mat);
         } else if (command == "rotate") {
-            double angle;
+            float angle;
             Point axis;
             scenefile >> angle >> axis;
             normalize(axis);
@@ -232,7 +233,7 @@ int main()
             Point c2 = rodrigues(j, axis, angle);
             Point c3 = rodrigues(k, axis, angle);
 
-            vec2d rot_mat(4, vector<double>(4, 0));
+            vec2d rot_mat(4, vector<float>(4, 0));
             rot_mat[0][0] = c1.x; rot_mat[0][1] = c2.x; rot_mat[0][2] = c3.x;
             rot_mat[1][0] = c1.y; rot_mat[1][1] = c2.y; rot_mat[1][2] = c3.y;
             rot_mat[2][0] = c1.z; rot_mat[2][1] = c2.z; rot_mat[2][2] = c3.z;
@@ -270,6 +271,7 @@ int main()
 
     ifstream stagefile;
     stagefile.open(nm_stage3file);
+    // stagefile.open("bin/stage.txt");
     if (!stagefile.is_open()) {
         std::cerr << "Problem opening the stage3 file!\n";
         exit(1);
@@ -294,23 +296,22 @@ int main()
 
         triangles.push_back(triangle);
     }
-    /* Provided output appears to have been made from triangles
-       taken in reverse order*/
-    reverse(triangles.begin(), triangles.end());
 
-    double dx = (lim_xr - lim_xl) / screen_width;
-    double dy = (lim_yt - lim_yb) / screen_height;
+    float dx = (lim_xr - lim_xl) / screen_width;
+    float dy = (lim_yt - lim_yb) / screen_height;
+    cout << dx << " " << dy << "\n";
 
-    double top_y = lim_yt - dy/2;
-    double left_x = lim_xl + dx/2;
+    float top_y = lim_yt - dy/2;
+    float left_x = lim_xl + dx/2;
 
     bitmap_image image(screen_width, screen_width);
     image.set_all_channels(0, 0, 0);
 
-    vec2d z_buffer(screen_width, vector<double>(screen_height, lim_zf));
+    vec2d z_buffer(screen_width, vector<float>(screen_height, lim_zr));
 
-    double x1,x2,x3;
-    double y1,y2,y3;
+    float x1,x2,x3;
+    float y1,y2,y3;
+    float z1,z2,z3;
 
     for (Triangle &t : triangles) {
         x1 = t.points[0].x;
@@ -319,49 +320,73 @@ int main()
         y1 = t.points[0].y;
         y2 = t.points[1].y;
         y3 = t.points[2].y;
+        z1 = t.points[0].z;
+        z2 = t.points[1].z;
+        z3 = t.points[2].z;
 
-        double max_y = max(max(y1, y2), y3);
-        max_y = (max_y >= top_y) ? top_y : ceil(max_y/dy)*dy - dy/2;
+        float min_y = min(min(y1, y2), y3);
+        float max_y = max(max(y1, y2), y3);
+        // cout << min_y << " " << max_y <<"\n";
 
-        double min_y = min(min(y1, y2), y3);
-        min_y = (min_y <= -top_y) ? -top_y : floor(min_y/dy)*dy + dy/2;
+        int min_r = (max_y >= top_y) ? 0 : ceil((top_y-max_y)/dy);
+        int max_r = (min_y <= -top_y) ? screen_height-1 : floor((top_y-min_y)/dy);
+        // cout << min_r << " " << max_r <<"\n";
 
-        // cout << top_y << " " << min_y << " " << max_y << " " << dy <<"\n";
-        double X1,X2,X3;
-        double min_x,max_x;
+        float X1,X2,X3;
+        float min_x,max_x;
 
-        for (double _y = min_y; _y < max_y; _y += dy) {
+        for (int row = min_r; row <= max_r; row++) {
+            float _y = top_y - row * dy;
             if (y1-y2 == 0) {
-                X2 = x2 + ((x2-x3)/(y2-y3)) * (_y - y2);
-                X3 = x3 + ((x3-x1)/(y3-y1)) * (_y - y3);
-                min_x = min(X2, X3);
-                max_x = max(X2, X3);
+                X1 = x2 + ((x2-x3)/(y2-y3)) * (_y - y2);
+                X2 = x3 + ((x3-x1)/(y3-y1)) * (_y - y3);
             } else if (y2-y3 == 0) {
                 X1 = x1 + ((x1-x2)/(y1-y2)) * (_y - y1);
-                X3 = x3 + ((x3-x1)/(y3-y1)) * (_y - y3);
-                min_x = min(X1, X3);
-                max_x = max(X1, X3);
+                X2 = x3 + ((x3-x1)/(y3-y1)) * (_y - y3);
             } else if (y3-y1 == 0) {
                 X1 = x1 + ((x1-x2)/(y1-y2)) * (_y - y1);
                 X2 = x2 + ((x2-x3)/(y2-y3)) * (_y - y2);
-                min_x = min(X1, X2);
-                max_x = max(X1, X2);
             } else {
                 X1 = x1 + ((x1-x2)/(y1-y2)) * (_y - y1);
                 X2 = x2 + ((x2-x3)/(y2-y3)) * (_y - y2);
                 X3 = x3 + ((x3-x1)/(y3-y1)) * (_y - y3);
-                min_x = min(min(X1, X2), X3);
-                max_x = max(max(X1, X2), X3);
+                if (X1 < min(x1,min(x2,x3)) || X1 > max(x1, max(x2,x3))) {
+                    swap(X1, X3);
+                } else if (X2 < min(x1,min(x2,x3)) || X2 > max(x1, max(x2,x3))) {
+                    swap(X2, X3);
+                } else if (X3 < min(x1,min(x2,x3)) || X3 > max(x1, max(x2,x3))) {
+                } else {
+                    cout << "Error detected!\n";
+                    exit(1);
+                }
             }
 
-            min_x = (min_x <= left_x) ? left_x : floor(min_x/dx)*dx + dx/2;
-            max_x = (max_x >= -left_x) ? -left_x : ceil(max_x/dx)*dx - dx/2;
-            // cout << left_x << " " << min_x << " " << max_x << " " << dx <<"\n";
+            min_x = min(X1, X2);
+            max_x = max(X1, X2);
 
-            for (double _x = min_x; _x < max_x; _x += dx) {
-                image.set_pixel(screen_width/2+_x*screen_width/2,
-                                screen_height/2-_y*screen_height/2,
-                                t.color[0],t.color[1],t.color[2]);
+            int min_c = (min_x <= left_x) ? 0 : round((min_x-left_x)/dx);
+            int max_c = (max_x >= -left_x) ? screen_width-1 : round((max_x-left_x)/dx);
+            // cout << min_c << " " << max_c <<"\n";
+
+            for (int col = min_c; col <= max_c; col++) {
+                float _x = left_x + col * dx;
+                float a1 = x2 - x1;
+                float b1 = y2 - y1;
+                float c1 = z2 - z1;
+                float a2 = x3 - x1;
+                float b2 = y3 - y1;
+                float c2 = z3 - z1;
+                float a = b1 * c2 - b2 * c1;
+                float b = a2 * c1 - a1 * c2;
+                float c = a1 * b2 - b1 * a2;
+                float d = (-a*x1 - b*y1 - c*z1);
+                float Z = (-a/c)*_x + (-b/c)*_y + (-d/c);
+                // cout << a << " " << b << " " << c << " " << d << " " << Z << "\n";
+
+                if (Z > lim_zf && Z < z_buffer[row][col]) {
+                    z_buffer[row][col] = Z;
+                    image.set_pixel(col, row, t.color[0],t.color[1],t.color[2]);
+                }
             }
             // break;
         }
@@ -369,9 +394,25 @@ int main()
         // break;
     }
 
-    image.save_image("output.bmp");;
+    ofstream zbuffile;
+    zbuffile.open(nm_zbuffile);
+    if (!zbuffile.is_open()) {
+        std::cerr << "Problem opening the stage3 file!\n";
+        exit(1);
+    }
+    zbuffile << setprecision(6) << fixed;
 
+    image.save_image("output.bmp");
+    for (auto &row : z_buffer) {
+        for (auto &col : row) {
+            if (col < lim_zr) {
+                zbuffile << col << "\t";
+            }
+        }
+        zbuffile << "\n";
+    }
 
+    zbuffile.close();
     conffile.close();
 
     return 0;
